ce1f628cb11588f638e9ab1e125987fd9942a8f7
Gopinath Langote <glangote@thoughtworks.com>
2017-03-10 23:35:43 +0530

    #000 - Gopinath - Added calculator

---
+++ src/main/scala/Calculator.scala
@@ -0,0 +1,10 @@
+case class Calculator() {
+  def add(x: Int, y: Int): Int = x + y
+
+  def subtract(x: Int, y: Int): Int = x - y
+
+  def multiply(x: Int, y: Int): Int = x * y
+
+  def divide(x: Int, y: Int): Int = x / y
+
+}
---
+++ src/test/scala/MathTest.scala
@@ -0,0 +1,25 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class MathTest extends FlatSpec with Matchers {
+  private val calculator = Calculator()
+
+  "The Add function" should "return 5 for adding 2, 3" in {
+    calculator.add(2, 3) shouldEqual 5
+  }
+
+  "The Subtract function" should "return 3 for subtracting 2 from 5" in {
+    calculator.subtract(5, 2) shouldEqual 3
+  }
+  "The Multiply function" should "return 6 for multiplying 2 and 3" in {
+    calculator.multiply(2, 3) shouldEqual 6
+  }
+
+  "The Divide function" should "return 2 for dividing 6 by 3" in {
+    calculator.divide(6, 3) shouldEqual 2
+  }
+  it should "throw Exception if diving number by zero" in {
+    a[ArithmeticException] should be thrownBy {
+      calculator.divide(6, 0)
+    }
+  }
+}
